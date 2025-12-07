368c2b0ee5c9fe1b54b1b55561fa35adf2870e4d
phischa <ph.git@mailbox.org>
2025-12-07 20:09:21 +0100

    feat: Add ApplicationResultMail mailable and email template

---
+++ resources/views/emails/application-result.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Application Result</title>
+</head>
+<body>
+    <h1>Hello {{ $application->name }},</h1>
+
+    @if($application->status === 'accepted')
+        <p>Congratulations! Your application to <strong>{{ $application->university->name }}</strong> has been accepted.</p>
+        <p>We look forward to welcoming you!</p>
+    @else
+        <p>Thank you for your interest in <strong>{{ $application->university->name }}</strong>.</p>
+        <p>Unfortunately, we are unable to offer you a place at this time.</p>
+        <p>We wish you the best in your future endeavors.</p>
+    @endif
+
+    <p>Best regards,<br>UniXplorer Team</p>
+</body>
+</html>
